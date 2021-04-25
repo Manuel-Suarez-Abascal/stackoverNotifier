e8a6b892a1d0ba315767293180eb2b02d2a6c336
Youssef <yt@papershift.com>
2021-04-25 15:26:24 +0200

    add UserQuestion component

---
+++ src/components/QuestionUser.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{ questionUser }}</div>
+</template>
+
+<script>
+export default {
+  name: "QuestionUser",
+  props: {
+    questionUser: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
