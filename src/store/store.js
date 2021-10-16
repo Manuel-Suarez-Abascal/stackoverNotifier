3a85e22b125f4b0499f163cfb5a18ade4e63f5c7
Manuel Abascal <manuelsuarezabascal@gmail.com>
2021-10-15 22:43:56 -0400

    add try/catch block

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,19 @@
 const axios = require("axios");
 
+/**
+ * @param {string} questionTag -> tag of the question to be fetched
+ * @returns {array} -> array of objects representing tag related questions from StackOverflow
+ */
 const getQuestions = async (questionTag) => {
-  return await axios
-    .get(
-      `https://api.stackexchange.com//2.2/questions?order=desc&sort=creation&tagged=${questionTag}&site=stackoverflow&key=sRtlAYT6ufs8EkbTzH3hlQ((`
-    )
-    .then((res) => res.data.items);
+  try {
+    return await axios
+      .get(
+        `https://api.stackexchange.com/2.2/questions?order=desc&sort=creation&tagged=${questionTag}&site=stackoverflow&key=sRtlAYT6ufs8EkbTzH3hlQ((`
+      )
+      .then((res) => res.data.items);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export { getQuestions };
