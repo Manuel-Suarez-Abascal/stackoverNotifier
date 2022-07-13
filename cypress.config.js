0305d135e80b91ab23cef336025544a5bf6396f6
Manuel Abascal <manuelabascal@Manuels-MacBook-Pro.local>
2022-07-13 04:38:59 +0300

    update cypress e2e & fix test suites

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  chromeWebSecurity: false,
+  projectId: "2u3ux8",
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require("./cypress/plugins/index.js")(on, config);
+    },
+    baseUrl: "http://localhost:3000/",
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
+  },
+};
